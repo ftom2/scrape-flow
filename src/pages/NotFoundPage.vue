c5c7dc2b4273f208e1f0396fd1ef2fe574acae61
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2024-11-03 23:34:52 +0200

    Added 404 page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Button from "@/components/ui/button/Button.vue";
+import { ArrowLeft } from "lucide-vue-next";
+</script>
+
+<template>
+  <div
+    class="fixed inset-0 z-50 flex flex-col items-center justify-center h-screen font-sans bg-background"
+  >
+    <h1 class="m-0 text-9xl">404</h1>
+    <p class="my-4 text-2xl">
+      Oops! The page you're looking for doesn't exist.
+    </p>
+    <router-link to="/" class="text-blue-500 hover:underline">
+      <Button>
+        <ArrowLeft class="w-6 h-6 mr-2" />
+        Back to Dashboard
+      </Button>
+    </router-link>
+  </div>
+</template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import HomePage from "@/pages/dashboard/DashboardPage.vue";
+import NotFoundPage from "@/pages/NotFoundPage.vue";
 
-const routes = [{ path: "/", component: HomePage }];
+const routes = [
+  { path: "/", component: HomePage },
+  { path: "/:catchAll(.*)*", component: NotFoundPage },
+];
 
 export const router = createRouter({
   history: createMemoryHistory(),
